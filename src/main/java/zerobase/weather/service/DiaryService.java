b08d998af08450537752741a1a72e90d52dd6e23
Kanggyeonggu <kku1031@naver.com>
2023-06-29 03:03:12 +0900

    받아온 데이터(json) 사용 가능하게 파싱하기 - 완료

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -3,6 +3,11 @@ package zerobase.weather.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 
 @Service
@@ -10,15 +15,40 @@ public class DiaryService {
 
     @Value("${openweathermap.key}")
     private String apiKey;
+
     public void createDiary(LocalDate date, String text) {
-        getWeatherString();
+        //open weather map에서 날씨 데이터 가져오기.
+        String weatherData = getWeatherString();
     }
 
     // open weather map API에서 데이터 받아오기
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Daegu&appid=" + apiKey;
-        System.out.println(apiUrl);
-        return "";
+
+        try {
+            URL url = new URL(apiUrl);
+            //http 형식으로 연결
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode(); //응답코드
+            BufferedReader br;  //오류 발생시, 오류 메시지가 길수 있으니 빠른 처리를 위해 BufferedReader
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            return response.toString();
+
+        } catch (Exception e) {
+            return "Response(응답)에 실패하였습니다.";
+        }
+
     }
     //받아온 날씨 데이터 파싱하기
 
